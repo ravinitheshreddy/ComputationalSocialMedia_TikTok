787c3d43dafd83901a181c1c82cfbb26241a6764
Elisa Michelet <elisa.michelet@epfl.ch>
2021-06-16 14:37:37 +0200

    change N hashtags

--- get_data.py
+++ get_data.py
@@ -8,7 +8,7 @@ verifyFp = "verify_kmj67wcs_9tsleZij_ZMtu_496d_9aJ1_vmKel1oWpLTW"
 
 hashtags = ['biden2020' , 'trump2020', 'biden', 'trump', 'election2020', '2020election', 'donaldtrump', 'joebiden', 'maga', 'trumpout', 'democrat', 'republican', 'trumpvsbiden', 'bidenvstrump', 'voteblue', 'votered']
 
-N_VIDEOS = 100
+N_VIDEOS = 500
 
 VIDEO_YEAR = 2020
 
@@ -22,7 +22,7 @@ def all_videos_one_hashtag(h):
         hashtags = set([c['title'] for c in v['challenges']])
         date = datetime.utcfromtimestamp(int(v['createTime'])).strftime('%Y-%m-%d %H:%M:%S')
         if datetime.strptime(date, '%Y-%m-%d %H:%M:%S').year == VIDEO_YEAR:
-            v_metadata = {'video_id': hash(v['id']),'hashtags': hashtags,'date': date,'user_id': hash(v['author']['id']),'n_likes':v['stats']['diggCount'], 'n_comments':v['stats']['commentCount'],'n_shared':v['stats']['shareCount'], 'sound': v['music']['title']}
+            v_metadata = {'video_id': hash(v['id']),'hashtags': hashtags,'date': date}
             tktks.append(v_metadata)
     return tktks
 
